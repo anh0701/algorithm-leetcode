0fb4aaf69d28af9a4a93259de4ae8300eb5e9e60
Pham Thi Quynh Anh <115700329+anh0701@users.noreply.github.com>
2024-02-16 22:08:43 +0700

    Time: 24 ms (22.97%), Space: 22.1 MB (5.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& v) {
-        int size = v.size(); /* returns the size of vector      TC - \U0001d4de(1)*/
+    void moveZeroes(vector<int>& nums) { 
         // vector<int> v2(nums.size());
         // remove_copy(nums.begin(), nums.end(), v2.begin(), 0);
-        v.erase( remove( v.begin(), v.end(), 0 ), v.end() );
         // nums.assign(v2.begin(), v2.end());
-        v.resize( size );
+        
+        // int size = nums.size();
+        // nums.erase( remove( nums.begin(), nums.end(), 0 ), nums.end() );
+        // nums.resize( size );
+        
+        queue<int> q;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                q.push(i);
+            }
+            else if(!q.empty()){
+                nums[q.front()] = nums[i];
+                nums[i] = 0;
+                q.pop();
+                q.push(i);
+            }
+        }
     }
 };
